767289d8f853d8e30544351d26fc63d0eeff9931
unknown <sheree.morphett@gmail.com>
2025-01-21 16:54:52 +0200

    Basic registering of cpp class as QML object

--- PintPath/pintbackend.cpp
+++ PintPath/pintbackend.cpp
@@ -1,3 +1,18 @@
 #include "pintbackend.h"
+#include <QDebug>
 
 PintBackend::PintBackend() {}
+
+bool PintBackend::isWorking() const
+{
+    qDebug() << "Is working:  " << m_isWorking;
+    return m_isWorking;
+}
+
+void PintBackend::setIsWorking(const bool &newIsWorking)
+{
+    if (m_isWorking == newIsWorking)
+        return;
+    m_isWorking = newIsWorking;
+    emit isWorkingChanged();
+}
