40489892fc69d36eff5dd12731e459d2cc8077ed
unknown <sheree.morphett@gmail.com>
2025-01-25 13:02:13 +0200

    Parser in place and correct

--- PintPath/pintbackend.h
+++ PintPath/pintbackend.h
@@ -5,6 +5,27 @@
 #include <QObject>
 #include <qqml.h>
 
+struct vendorData
+{
+    QString id;
+    QString name;
+    QString brewery_type;
+    QString address_1;
+    QString address_2;
+    QString address_3;
+    QString city;
+    QString state_province;
+    QString post_code;
+    QString country;
+    double longitude;
+    double latitude;
+    QString phone;       //TODO could have that open the phone app to call?
+    QString website_url; //TODO could have the hyperlink work?
+};
+
+// https://stackoverflow.com/questions/2677577/how-to-overload-operator-for-qdebug
+QDebug &operator<<(QDebug &debug, const vendorData &data);
+
 class PintBackend : public QObject
 {
     Q_OBJECT
@@ -18,20 +39,28 @@ public:
 
     Q_INVOKABLE void sendRequest(const QString &endpoint, const QString &criteria);
 
+    vendorData findingNorthern();
+    vendorData findingSouthern();
+    vendorData findingLongestName();
+
 signals:
     void isWorkingChanged();
-    void apiResponseReceived(const QString &criteria, const QString &response);
+    void apiResponseReceived(const QString &criteria, const QByteArray &response);
     void apiErrorOccurred(const QString &criteria, const QString &error);
 
     //TODO: Gain a better understanding of slots and signals
 private slots:
-    void handleApiResponse(const QString &criteria, const QString &response);
+    void handleApiResponse(const QString &criteria, const QByteArray &response);
+    // void handleApiResponse(const QString &criteria, const QString &response);
     void handleApiError(const QString &criteria, const QString &error);
 
 private:
+    std::vector<vendorData> vendorDatabase;
     bool m_isWorking;
     QNetworkAccessManager networkManager;
     QString currentCriteria;
+
+    void populateDatabase(const QByteArray &response);
 };
 
 #endif // PINTBACKEND_H
